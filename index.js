b5a77e7121930334324a5431a0151a4e1929644c
somtochukwu nwosu <somynwosu@gmail.com>
2022-11-07 12:37:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let dataRender = document.querySelector('#data-render');
+function generateUser(){
+    fetch('https://randomuser.me/api/')
+    .then((response) => response.json())
+    .then((data) =>{
+        console.log(dataRender);
+        dataRender.innerHTML = `<div> 
+        <h1 class ="name-text">
+        ${data?.results[0]?.name?.title}
+        ${data?.results[0]?.name?.first}
+        ${data?.results[0]?.name?.last}
+        </h1>
+        <p class="para-text"> ${data?.results[0].email} </p>
+         
+        <img class="image" src = ${data?.results[0].picture?.large}
+        </div>`
+    console.log(data)})
+}
+
